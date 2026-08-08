ca759648b371f767af10635d11312c38b2e8aba7
Jacob Manzano <jacob.m@megacatstudios.com>
2026-08-09 01:39:33 +0800

    Temporarily Adjusted DamageEffect to be the One to Make Use of Selected Targets

--- Assets/Scripts/Combat/Effect/DamageEffect.cs
+++ Assets/Scripts/Combat/Effect/DamageEffect.cs
@@ -13,6 +13,18 @@ public class DamageEffect : Effect
         // From there it should resolve what to target in the battle param.
         // May need to have targets as array in any case the move hits multiple people.
 
-        battle.Attacker.GetTarget(battle).UpdateHP(-damage);
+        //battle.Attacker.GetTarget(battle).UpdateHP(-damage);
+        
+        // TODO: We'll need to figure out at what level of the skill the target gets chosen.
+        // I believe that it should probably maybe at the skill level (or maybe the attempt level),
+        // depending on how we sequence the skills (like from an animation). Could also be in attempt.
+        if (battle.Attacker == null)
+            return;
+
+        if (battle.Targets == null || battle.Targets.Count == 0)
+            return;
+
+        foreach (var target in battle.Targets)
+            target.UpdateHP(-damage);
     }
 }
